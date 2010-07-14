7eb47dc80aa771e9db3d73e50c1bda65a50dfba6
Ranjit Jhala <jhala@ubuntu.(none)>
2010-07-13 19:03:16 -0700

    foo..

--- hornToInterproc.ml
+++ hornToInterproc.ml
@@ -6,6 +6,11 @@ module SM = Ast.Symbol.SMap
 module H  = Ast.Horn
 open Misc.Ops
 
+
+(***************************************************************************)
+(************************* Parse Horn Clauses ******************************)
+(***************************************************************************)
+
 let tmpname = "tmp"
 
 let clean f : unit =
@@ -19,22 +24,52 @@ let clean f : unit =
   end
 
 let parse f : H.t list = 
-  let _  = Errorline.startFile f in
+  let _  = clean f in
+  let _  = Errorline.startFile tmpname in
   let ic = open_in f in
   let rv = Lexing.from_channel ic |> HornParse.horns HornLex.token in
   let _  = close_in ic in
   rv
 
-let process f: H.t list =
-  clean f;
-  parse tmpname
+(****************************************************************************)
+(******************* Preprocess HC to get global information ****************)
+(****************************************************************************)
 
-let dump cs = 
-  Format.printf "%a\n\n\n" (Misc.pprint_many true "\n" H.print) cs
+k -> arity
+k -> local vars
+k -> call vars
+
+k(x1,x2,x3) = a1 := x1, a2 := x2, a3 := x3; k(a1, a2, a3)
 
 let arities_of_ts (cs: H.t list) : int SM.t = failwith "TBD"
 
-let interproc_of_ts (cs: H.t list) : string = failwith "TBD"
+
+(****************************************************************************)
+(******************* Preprocess HC to get global information ****************)
+(****************************************************************************)
+
+let tx_gd (
+
+let tx_hd (h: H.pr) : string = failwith "TBD"
+
+let tx_t (c: H.t) : string = failwith "TBD"
+
+let tx_ts (k: string, cs: H.t list) : string = failwith "TBD"
+
+let tx cs = 
+
+1. gather arities of each k
+2. cluster constraints by k
+3. translate constraint list
+        - translate guards
+        - translate head
+
+(***************************************************************************)
+(***************************** Output Clauses ******************************)
+(***************************************************************************)
+
+let dump cs = 
+  Format.printf "%a\n\n\n" (Misc.pprint_many true "\n" H.print) cs
 
 let usage = "hornToInterproc.native filename.pl"
 
@@ -48,6 +83,6 @@ let main usage =
   print_now "\n========================================================\n";
   let fs = ref [] in
   let _  = Arg.parse Co.arg_spec (fun s -> fs := s::!fs) usage in
-  !fs |> Misc.flap process |> dump 
+  !fs |> Misc.flap parse |> dump 
 
 let _ = main usage
