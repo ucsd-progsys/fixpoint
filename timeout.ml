a37b087591759f4bf01b064b82aa8b5d438c24b1
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2011-01-31 21:03:50 -0800

    clean up the timeout wrapper

--- timeout.ml
+++ timeout.ml
@@ -5,19 +5,28 @@ module T = Thread
 
 let get_time () = int_of_float (Unix.time ())
 
-let do_timeout i f x =
-  let g x lk (ret, rd) =
+let mk_task f x =
+  let task x lk (ret, rd) =
     let rv = f x in
     M.lock lk; ret := Some rv; rd := true; M.unlock lk in
+  task x
+
+let not_done lk (ret, rd) = 
+  M.lock lk; let trd = !rd in (M.unlock lk; not(trd))
+
+let fail thread (ret, rd) =
+  T.kill thread; ret := None; rd := true 
+
+let do_timeout i f x =
+  let task = mk_task f x in
   let (ret, rd) as rr = (ref None, ref false) in
   let stime = get_time () in
   let lk = M.create () in 
-  let t  = T.create (g x lk) rr in
-  while (M.lock lk; let trd = !rd in (M.unlock lk; not(trd))) do
+  let t  = T.create (task lk) rr in
+  while not_done lk rr do
     if (get_time () - stime < i) then
       T.yield ()
     else
-      (T.kill t; ret := None; rd := true)
+      fail t rr
   done; !ret
-
 
